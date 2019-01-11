86d66a5619ec5d0c8eda7754d64b9483ef65d5cb
quochung <hungfeck@gmail.com>
2019-01-11 15:27:46 +0700

    add create server

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-
+var http = require('http');
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(port, () => console.log(`Example app listening on port ${port}!`));
